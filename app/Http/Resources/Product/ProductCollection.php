ed1e8047bc98790dd43e753d59778fdf66577415
Samuel R <samuel_-_rojas@hotmail.com>
2019-06-12 16:47:36 -0500

    Product collection resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,7 +14,17 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+          'name' => $this->name,
+          'totalPrice' => (1 - bcdiv($this->discount, 100, 2))* $this->price,
+          'rating' => $this->reviews->count() > 0
+              ? (float) bcdiv($this->reviews->sum('star'), $this->reviews->count(), 2)
+              : 'No rating yet',
+          'discount' => $this->discount,
+          'href' => [
+            'link' => route('products.show', $this->id),
+          ],
+        ];
     }
 }
 /**
