4c64e393f08e0b7f953889d143ea602329bf08a9
Samuel R <samuel_-_rojas@hotmail.com>
2019-06-12 15:20:20 -0500

    Add relationship for models Reviews/Products

--- app/Entities/Product.php
+++ app/Entities/Product.php
@@ -3,8 +3,12 @@
 namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Entities\Review;
 
 class Product extends Model
 {
-    //
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Entities/Review.php
+++ app/Entities/Review.php
@@ -3,8 +3,12 @@
 namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Entities\Product;
 
 class Review extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
