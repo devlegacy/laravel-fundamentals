fc4e2f78c36f73e5062ea296483b51d63ca33ad1
Samuel R <samuel_-_rojas@hotmail.com>
2019-06-17 01:45:03 -0500

    Update packages, create commands in composer and add Api\UserController

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get('/user', 'Api\UserController@index');
 
 Route::apiResource('products', 'Api\ProductController');
 Route::group(['prefix'=>'products'], function () {
